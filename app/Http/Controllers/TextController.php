8bc87b23819ee6e87f56ca5df26f2f05dd17f1a7
Har8 <harutharutunyan94@gmail.com>
2020-10-05 11:41:38 +0400

    select

--- app/Http/Controllers/TextController.php
+++ app/Http/Controllers/TextController.php
@@ -7,53 +7,52 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 
-
-
-
 class TextController extends Controller
 {
     public function getword($arr)
     {
-        $testword = 'hello';
-        for ($i = 0; $i < count($arr); $i++)
-        {
-            if ($arr[$i] == '' || $arr[$i] == ',' || $arr[$i] == ':' || $arr[$i] == '.')
-            {
+        $testword = 'Error';
+        $newarr = [];
+        for ($i = 0; $i < count($arr); $i++) {
+            if ($arr[$i] == '' || $arr[$i] == ',' || $arr[$i] == ':' || $arr[$i] == '.') {
                 unset($arr[$i]);
-            }
-            else
-            {
-                $count = 0;
+            } else {
                 $word = $arr[$i];
-                $newarr = [];
                 $dbwords = DB::select('select word from words');
-              //  dd($dbwords);
-                for ($j = 0; $j < count($dbwords); $j++)
-                {
-                    foreach ($dbwords[$j] as $key => $value)
-                    {
+                //  dd($dbwords);
+                for ($j = 0; $j < count($dbwords); $j++) {
+                    foreach ($dbwords[$j] as $key => $value) {
 
                         $textword = $word;
                         $test = similar_text($textword, $value, $percent);
 
-                        if ($percent > 80)
-                            {
-                                $newarr[]= $value;
-                            }
+                        if ($percent == 100) {
+                            return "";
+                        }
+                        else if ($percent >= 80 && $percent < 100) {
+
+                            $newarr[] = $value;
+                        }
                     }
 
                 }
             }
         }
+//        dd($newarr);
+//        return view('home', compact('newarr'));
+
+//        print_r($newarr);
+
+//        return response($newarr);
         return $newarr;
     }
 
     public function text(Request $request)
     {
-//        $info = Textarea::get('description');
         $text = $request->get('myword');
         $arr = explode(" ", $text);
         $trueWord = $this->getword($arr);
-        return $trueWord;
+       // dd($trueWord);
+        return response()->json(['trueWord'=>$trueWord]);
     }
 }
